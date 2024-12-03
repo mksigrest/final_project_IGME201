920869db51e69610d551a333daffaadfd9c1e826
mksigrest <mksigrest@gmail.com>
2024-12-03 09:11:18 -0500

    push 1
    initial push for the final project.
    Included all project members, leads, work to do, and soft deadlines for work to be done.

---
+++ final-project-IGME201/final-project-IGME201/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Taco bell screen ordering system. Images and names with food items. Last ordered item
+ * pulled up with I/O.
+ * 
+ *  Jacob Duryea - Design Lead - Design Layout & Program Layout
+ *  Mariana Espinal - Commenting Lead - File I/O
+ *  Max Sigrest - Co-Lead - Ordering & Recipt System
+ *  Noah Wexler - Co-lead - Design Layout & Program Layout
+ *  Saavan Tandon - Debugging Lead - Classes
+ *  
+ *  1: Design Layout & Program Layout - 12/7/24
+ *  2: Classes - 12/10/24
+ *  3: Ordering & Recipt System - 12/11/24
+ *  4: File I/O - 12/14/24
+ */
+
+namespace final_project_IGME201
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
